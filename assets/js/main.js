a5aa48c6b1c05cc882ceca584ab5faee4c87fc8f
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-05-12 00:11:19 +0600

    feat(services): add accordion functionality for service cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,6 +79,28 @@ const servicesButton = document.querySelectorAll('.services__button')
 servicesButton.forEach((button) => {
     // Add your height to services info
     const heightInfo = document.querySelector('.services__info')
+    heightInfo.style.height = heightInfo.scrollHeight + 'px'
+
+    button.addEventListener('click', () => {
+        const servicesCard = document.querySelectorAll('.services__card'),
+                currentCard = button.parentNode
+                currentInfo = currentCard.querySelector('.services__info')
+                isCardOpen = currentCard.classList.contains('services-open')
+
+        // Close all other services info
+        servicesCard.forEach((card) => {
+            card.classList.replace('services-open', 'services-close')
+
+            const info = card.querySelector('.services__info')
+            info.style.height = 0
+        })
+
+        // Open only if not already open
+        if (!isCardOpen) {
+            currentCard.classList.replace('services-close', 'services-open')
+            currentInfo.style.height = currentInfo.scrollHeight + 'px'
+        }
+    })
 
 })
 
